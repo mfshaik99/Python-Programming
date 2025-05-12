62469248f3b4c7b2fb83fcdc211e2aebcf651a1f
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-05-12 14:41:53 +0530

    main.py

---
+++ Modules and Packages/mian.py
@@ -0,0 +1,11 @@
+# Using built-in and external modules.
+
+# Importing math module
+import math
+
+# Using sqrt function from math module
+print(f"Square root of 16: {math.sqrt(16)}")
+
+# Importing specific function
+from math import pi
+print(f"Value of Pi: {pi}")
