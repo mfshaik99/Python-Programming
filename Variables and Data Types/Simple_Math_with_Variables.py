ddebc30488344670deb4c4806e97e1539fdf9cbc
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-05-12 14:48:45 +0530

    Simple_Math_with_Variables.py

---
+++ Variables and Data Types/Simple_Math_with_Variables.py
@@ -0,0 +1,15 @@
+# Defining variables
+num1 = 10
+num2 = 5
+
+# Performing simple math operations
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+# Printing the results
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
