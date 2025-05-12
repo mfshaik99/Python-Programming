14b70965447a8dc859c62cc01834160fca437c62
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-05-12 14:54:42 +0530

    Simple_Data_Formatter_Using_Operators.py

---
+++ Basic Operators/Simple_Data_Formatter_Using_Operators.py
@@ -0,0 +1,13 @@
+# Input user data
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# String and arithmetic operations
+doubled_name = name * 2     # repeats the name
+age_in_5_years = age + 5    # adds 5 to age
+name_length = len(name)     # counts characters in name
+
+# Output
+print("Your name twice:", doubled_name)
+print("Your age after 5 years:", age_in_5_years)
+print("Number of characters in your name:", name_length)
