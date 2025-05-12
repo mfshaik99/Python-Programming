a7225c9213e3e6c35f097f9266a8dc7debf035d1
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-05-12 14:58:33 +0530

    Traffic_signal.py

---
+++ Conditional Statements /Traffic_signal.py
@@ -0,0 +1,10 @@
+signal = input("Enter signal color (red/yellow/green): ")
+
+if signal == "red":
+    print("Stop!")
+elif signal == "yellow":
+    print("Get ready.")
+elif signal == "green":
+    print("Go!")
+else:
+    print("Invalid signal.")
