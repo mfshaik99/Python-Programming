0150e3af5d0fe2a16fcfda7b777e8a5e9be6d1b9
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-05-12 14:44:39 +0530

    main.py

---
+++ Iterators/main.py
@@ -0,0 +1,9 @@
+# Iterators allow traversing through collections
+
+# Iterator
+numbers = [1, 2, 3, 4]
+iterator = iter(numbers)
+
+# Iterating through the list using iterator
+print(next(iterator))  # 1
+print(next(iterator))  # 2
