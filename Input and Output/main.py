5ed5fabbe9d9d1a9aa3e0b4341b8571c827157c7
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-05-12 14:20:46 +0530

    Delete Input and Output/main.py

--- Input and Output/main.py
+++
@@ -1,8 +0,0 @@
-# Input and Output
-# This program takes user input and prints it back.
-
-name = input("Enter your name: ")  # Accepting user input
-age = int(input("Enter your age: "))  # Converting input to integer
-
-# Display the message with user input
-print(f"Hello {name}, you are {age} years old.")
