f9e361fe9965e57114c12b2a128bf0d75b6a8a72
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-05-12 14:10:42 +0530

    Create Basic_Operators.py

---
+++ Basic_Operators.py
@@ -0,0 +1,11 @@
+# Basic Operators
+a = 10
+b = 5
+
+print(f"Sum: {a + b}")
+print(f"Difference: {a - b}")
+print(f"Product: {a * b}")
+print(f"Quotient: {a / b}")
+print(f"Modulu Division: {a % b}")
+print(f"Exponential: {a ** b}")
+print(f"Floor: {a // b}")
