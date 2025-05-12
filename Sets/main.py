f83eb1c5d344f073231abec034531810cf0a873d
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-05-12 14:37:12 +0530

    main.py

---
+++ Sets/main.py
@@ -0,0 +1,10 @@
+#Sets are unordered collections with unique elements.
+# Set in Python
+unique_numbers = {1, 2, 3, 4, 4}
+
+# Adding elements
+unique_numbers.add(5)
+print(f"Updated set: {unique_numbers}")
+
+# Checking if an element exists in a set
+print(f"Is 3 in set: {3 in unique_numbers}")
