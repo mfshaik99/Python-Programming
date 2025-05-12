8186e2e9d5f03af22c6bd407de746bdf7cf2061b
MUHAMMAD FARHAN SHAIK <24B81A66E3@cvr.ac.in>
2025-05-12 14:47:19 +0530

    Variable_Assignment.py

---
+++ Variables and Data Types/Variable_Assignment.py
@@ -0,0 +1,11 @@
+# Assigning values to variables
+name = "John"      # String data type
+age = 25           # Integer data type
+height = 5.9       # Float data type
+is_student = True  # Boolean data type
+
+# Displaying the values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
